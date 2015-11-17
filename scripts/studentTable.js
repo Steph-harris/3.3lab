5cce2bc39bbd1835746ac1ad3b5c9e691f4528ad
steph-harris <steph.harris21@gmail.com>
2015-11-17 17:08:58 -0500

    added insertRow method to JS

--- scripts/studentTable.js
+++ scripts/studentTable.js
@@ -21,10 +21,17 @@ function deleteRow(){
 var addRows = document.getElementById("adding");
   addRows.addEventListener("click", addRow);
 // function that adds a row to the table
-debugger;
 function addRow(){
-  //deleteButtons[0].parentNode.parentElement.appendChild();
-  deleteButtons.parentElement.appendChild(adding);
+  debugger;
+  var row = mainTab.insertRow(-1);
+  var cell1 = row.insertCell (0);
+  var cell2 = row.insertCell (1);
+  var cell3 = row.insertCell (2);
+  var cell4 = row.insertCell (3);
+  cell1.innerHTML = "firstName";
+  cell2.innerHTML = "lastName";
+  cell3.innerHTML = "email";
+  cell4.innerHTML = "deleteButtons[0]"
 }
 
 //timer that randomly between 5-10 secs deletes rows
